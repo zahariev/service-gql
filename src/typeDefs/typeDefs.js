9a70ae6c0142011bee9092562f375e70511d6161
Boyan Zahariev <bzahariev@soft2run.com>
2022-01-25 14:58:38 +0200

    fix: typeDef

--- src/typeDefs/typeDefs.js
+++ src/typeDefs/typeDefs.js
@@ -1,10 +1,16 @@
 const { gql } = require("apollo-server-express");
 // const user = require("./user");
 // const post = require("./post");
+
 const geolocation = require("./geolocation");
 
 const base = gql`
   type PingResponse {
+    message: String
+    _version: Int
+  }
+
+  type GeoResponse {
     message: String
     _version: Int
   }
